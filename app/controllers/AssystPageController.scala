ca35f25d0b761638baded6914c73005cbde4fd74
brendan.straughan <brendan.straughan@digital.hmrc.gov.uk>
2020-05-07 10:39:24 +0100

    Assyst Page | Basic shell for controller, view and unit tests added in and working, all unit tests passing

--- app/controllers/AssystPageController.scala
+++ app/controllers/AssystPageController.scala
@@ -20,14 +20,14 @@ import javax.inject.Inject
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
-import views.html.SessionExpiredView
+import views.html.AssystPageView
 
-class SessionExpiredController @Inject()(
-                                          val controllerComponents: MessagesControllerComponents,
-                                          view: SessionExpiredView
-                                        ) extends FrontendBaseController with I18nSupport {
+class AssystPageController @Inject()(
+                                    val controllerComponents: MessagesControllerComponents,
+                                    view: AssystPageView
+                                  ) extends FrontendBaseController with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = Action { implicit request =>
+  def onPageLoad():Action[AnyContent] = Action { implicit request =>
     Ok(view())
   }
 }
