2ce82129a4f1f4b34dc78e20df71597f71bad3a0
Dandyboyo2 <45761631+Dandyboyo2@users.noreply.github.com>
2020-05-14 13:53:38 +0100

    changes made to the second page in the journey

--- test/views/RequestLiveServiceInvestigationViewSpec.scala
+++ test/views/RequestLiveServiceInvestigationViewSpec.scala
@@ -1,4 +1,4 @@
-@*
+/*
  * Copyright 2020 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,19 +12,23 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *@
+ */
 
-@this(
-main_template: MainTemplate
-)
+package views
 
-@()(implicit request: Request[_], messages: Messages)
+import views.behaviours.ViewBehaviours
+import views.html.RequestLiveServiceInvestigationView
 
-@main_template(
-title = messages("request.live.service.investigation.title")
- ) {
+class RequestLiveServiceInvestigationViewSpec extends ViewBehaviours {
 
-    @components.back_link()
+  "RequestLiveServiceInvestigation view" must {
 
-    @components.heading("request.live.service.investigation.heading")
+    val view = viewFor[RequestLiveServiceInvestigationView](Some(emptyUserAnswers))
+
+    val applyView = view.apply()(fakeRequest, messages)
+
+    behave like normalPage(applyView, "requestLiveServiceInvestigation")
+
+    behave like pageWithBackLink(applyView)
+  }
 }
